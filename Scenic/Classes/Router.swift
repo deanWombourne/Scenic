b6b252f87eccfaf1429df916e38e24b9b55e3dce
Sam Dean <deanWombourne@gmail.com>
2021-06-25 16:47:19 +0100

    Add some helper convenience methods to Router

--- Scenic/Classes/Router.swift
+++ Scenic/Classes/Router.swift
@@ -19,3 +19,30 @@ open class Router<ViewController: UIViewController, FactoryContext> {
         self.factory = factory
     }
 }
+
+extension Router {
+
+    private func push(_ controller: @autoclosure () -> UIViewController, animated: Bool) -> Bool {
+        guard let navigationController = self.controller?.navigationController else { return false }
+        navigationController.pushViewController(controller(), animated: animated)
+        return true
+    }
+
+    /// Push a view controller onto the current view controller's navigation controller
+    ///
+    /// - parameter animated: _(optional)_ Animate the push, defaults to `true`
+    /// - returns: `false` if a navigation controller could not be found. `true` otherwise.
+    @discardableResult
+    public func pushViewController(_ controller: UIViewController, animated: Bool = true) -> Bool {
+        self.push(controller, animated: animated)
+    }
+
+    /// Push a scene onto the current view controller's navigation controller
+    ///
+    /// - parameter animated: _(optional)_ Animate the push, defaults to `true`
+    /// - returns: `false` if a navigation controller could not be found. `true` otherwise.
+    @discardableResult
+    public func pushScene<T: Scene>(_ scene: @autoclosure () -> T, animated: Bool = true) -> Bool where T.FactoryContext == FactoryContext {
+        self.push(factory.createViewController(scene()), animated: animated)
+    }
+}
